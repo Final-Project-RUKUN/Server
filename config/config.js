93ccb88892776851ccc6a091022b2f73bb95d9a0
caturprasatya <caturprasatya1224@gmail.com>
2021-04-20 14:31:50 +0700

    validation for deploy aws

--- config/config.js
+++ config/config.js
@@ -17,7 +17,11 @@ module.exports = {
   "development": database,
   "test": database,
   "production": {
-    "use_env_variable" : "DATABASE_URL",
+    "username": 'ykntypxwbhpudf',
+    "password": '787df0dfcb8474ff684b9c2b94db07c9f010916025c354c79a0f34a93af4ef29',
+    "database": 'd1uqkihop6895c',
+    "host": 'ec2-54-220-35-19.eu-west-1.compute.amazonaws.com',
+    "dialect": "postgres",
     "dialectOptions" : {
       "ssl": {
           "rejectUnauthorized": false
--- helpers/useJwt.js
+++ helpers/useJwt.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken')
 
 const generateToken = (payload) => {
-  return jwt.sign(payload, process.env.KEY)
+  return jwt.sign(payload, 'guyubRUKUN')
 }
 
 const verifyToken = (token) => {
-  return jwt.verify(token, process.env.KEY)
+  return jwt.verify(token, 'guyubRUKUN')
 }
 
 module.exports = { generateToken, verifyToken }
